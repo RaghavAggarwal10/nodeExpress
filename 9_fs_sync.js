c9d51f299e77ecd8b1079a7c7d05a2b7f7d3dbb2
RaghavAggarwal10 <aggarwal.raghav170@gmail.com>
2024-06-21 16:01:20 +0530

    sync_file

--- 9_fs_sync.js
+++ 9_fs_sync.js
@@ -7,3 +7,4 @@ console.log(first,second)
 writeFileSync('./content/third.txt',`Here is the reult ${first},${second}`)//rember if the file is not present node creates the file 
 //also it takes 2 parameters one is file path  and other one is the text which we want to add and if the file is already there thennode will overwrite the file and if we want to append it in the file we can 
 //just pass a third paramete {flag:'a'}
+writeFileSync('./content/third.txt',`Here is the reult ${first},${second}`,{flag:'a'})// this will append in the existing file 
