08cdffb80a291609237317095a6b7a98a268b5c7
RaghavAggarwal10 <aggarwal.raghav170@gmail.com>
2024-06-21 16:00:41 +0530

    sync_file

---
+++ 9_fs_sync.js
@@ -0,0 +1,9 @@
+const {readFileSync, writeFileSync}=require('fs')
+// const fs=require('fs')
+// fs.readFile alternate waay to do this 
+const first= readFileSync('./content/first.txt','utf-8')//it takes 2 inputs one is encoding and the other one is the file path  
+const second= readFileSync('./content/second.txt','utf-8')//it takes 2 inputs one is encoding and the other one is the file path  
+console.log(first,second)
+writeFileSync('./content/third.txt',`Here is the reult ${first},${second}`)//rember if the file is not present node creates the file 
+//also it takes 2 parameters one is file path  and other one is the text which we want to add and if the file is already there thennode will overwrite the file and if we want to append it in the file we can 
+//just pass a third paramete {flag:'a'}
